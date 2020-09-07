c918a104edc4b1e57feb745e2d3e1d49a8f25f46
Anna Morris <annapatton444@gmail.com>
2020-09-07 12:23:51 -0500

    searching

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,5 +1,7 @@
 def linear_search(arr, target):
-    # Your code here
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
 
     return -1   # not found
@@ -7,8 +9,18 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
-    # Your code here
+    start = 0 
+    end = (len(arr)-1)
+    
+    while end >= start:
+        middle = start + end //2
+        if arr[middle] == target:
+            return middle
+        else:
+            if arr[middle] > target:
+                end = middle - 1
+            if arr[middle] < target:
+                start = middle + 1
 
 
     return -1  # not found
